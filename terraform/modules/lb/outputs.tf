e545f5d9a3ad6fa6234689625ccae878111b000d
maximus7022 <pash6790@gmail.com>
2023-10-10 19:24:15 +0300

    Adding necessary outputs

--- terraform/modules/lb/outputs.tf
+++ terraform/modules/lb/outputs.tf
@@ -5,3 +5,11 @@ output "load_balancer_arn" {
 output "target_group_arn" {
   value = aws_lb_target_group.app_tg.arn
 }
+
+output "alb_dns_name" {
+  value = aws_lb.lb.dns_name
+}
+
+output "alb_zone_id" {
+  value = aws_lb.lb.zone_id
+}
